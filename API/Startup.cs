e2b3b12f4ea91c3b260450a6d7795a443c19d701
ashishlasinkar <ashishlasinkar@gmail.com>
2021-05-18 12:35:12 +0530

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -21,7 +21,7 @@ namespace API
         private readonly IConfiguration _config;
         public Startup(IConfiguration config)
         {
-            _config= config;
+            _config = config;
         }
 
         public IConfiguration Configuration { get; }
@@ -38,6 +38,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +54,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
